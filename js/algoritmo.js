032b0e3b15fd786580cedee1c78732f09b61c04d
Yinner Chilito <yinnermedina30@gmail.com>
2022-12-20 20:02:55 -0500

    comentarios funciones algoritmo de encriptación/desencriptación

--- js/algoritmo.js
+++ js/algoritmo.js
@@ -1,4 +1,10 @@
+//---Funciones necesarias para encriptar una cadena de texto
+
 function encriptarLetra(letra) {
+    /* 
+    Se recibe una sola letra y se retorna el valor clave que corresponde de acuerdo
+    a la vocal obtenida, si es consonante se retorna el mismo valor.
+     */
     switch (letra) {
         case "a":
             return "ai";
@@ -21,6 +27,11 @@ function encriptarLetra(letra) {
 }
 
 function encriptarTexto(texto) {
+    /* 
+    Se recorre la cadena de texto y con ayuda de la funcion de "encriptarLetra" se
+    logra encriptar cada vocal que se encuentre en el recorrido.
+    Posteriormente se añade cada resultado retornado por la funcion en una nueva cadena.
+     */
     var textoEncriptado = "";
     for (var i = 0; i < texto.length; i++) {
         var letra = texto[i];
@@ -30,7 +41,12 @@ function encriptarTexto(texto) {
     return textoEncriptado;
 }
 
+//---Funciones necesarias para desencriptar una cadena de texto
 function desencriptarPalabra(letra, letraEncriptada) {
+    /* 
+    Se recibe la palabra y de acuerdo a las conversiones obtenidas de la anterior función, se remplazan los valores por los originales en
+    todas las coincidencias encontradas en la palabra.
+    */
     var palabraOriginal = letra;
 
     if (letraEncriptada.includes(1)) {
@@ -52,6 +68,10 @@ function desencriptarPalabra(letra, letraEncriptada) {
 }
 
 function concatenarPalabras(lista) {
+    /* 
+    Una vez completado el procesos de remplazo de valores, se reunen las palabras de la lista, para unirlas en una nueva cadena, con los
+    valores correctos.
+     */
     var oracion = "";
     for (var i = 0; i < lista.length; i++) {
         oracion += lista[i] + " ";
@@ -60,6 +80,9 @@ function concatenarPalabras(lista) {
 }
 
 function validarEncriptadoPalabra(palabra) {
+    /* 
+    Se identifican las conversiones en la palabra, para identificar la vocal que fue afectada y retornar el codigo para este caso en particular.
+    */
     var resultado = [];
     var estado = false;
     palabra.includes("ai") ? resultado.push(1) : estado = false;
@@ -71,6 +94,13 @@ function validarEncriptadoPalabra(palabra) {
 }
 
 function desencriptarTexto(texto) {
+    /*
+    Se separan las palabras de la cadena en una lista para luego recorrerlas, para
+    validar las palabras que estan encriptadas y así desencriptarlas con la función "desencriptarPalabra"
+    y almacenar la palabra en una lista, para luego concatenarlas, formando la cadena con el texto
+    desencriptado.
+
+    */
     var listaPalabras = entrada.split(" ");
     var listaPalabrasDesencriptadas = [];
     var letrasEncriptadas = [];
